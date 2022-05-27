b932f9972f5ef6ded3e7631dba45566199ae6c30
HuskyNator <HuskyNator@protonmail.ch>
2022-05-28 01:00:57 +0200

    Quaternions

--- source/app.d
+++ source/app.d
@@ -22,9 +22,12 @@ void main() {
 }
 
 class Speler : Voorwerp {
+	private Quat xdraai;
+	private Quat ydraai;
 
-	private Vec!3 verplaatsing;
-	private Vec!3 draaiing;
+	private Vec!3 _verplaatsing;
+	private Vec!2 _draai;
+	private Vec!2 _draaiDelta;
 	nauwkeurigheid snelheid = 3;
 	nauwkeurigheid draaiSnelheid = 0.6;
 
@@ -41,22 +44,22 @@ class Speler : Voorwerp {
 		int delta = (invoer.gebeurtenis == GLFW_PRESS) ? 1 : -1;
 		switch (invoer.toets) {
 		case GLFW_KEY_A:
-			verplaatsing.x -= delta;
+			_verplaatsing.x -= delta;
 			break;
 		case GLFW_KEY_D:
-			verplaatsing.x += delta;
+			_verplaatsing.x += delta;
 			break;
 		case GLFW_KEY_SPACE:
-			verplaatsing.y += delta;
+			_verplaatsing.y += delta;
 			break;
 		case GLFW_KEY_LEFT_CONTROL:
-			verplaatsing.y -= delta;
+			_verplaatsing.y -= delta;
 			break;
 		case GLFW_KEY_S:
-			verplaatsing.z += delta;
+			_verplaatsing.z -= delta;
 			break;
 		case GLFW_KEY_W:
-			verplaatsing.z -= delta;
+			_verplaatsing.z += delta;
 			break;
 		default:
 		}
@@ -65,10 +68,8 @@ class Speler : Voorwerp {
 	void muisinvoer(MuisplekInvoer invoer) nothrow {
 		static double oude_x = 0;
 		static double oude_y = 0;
-		double delta_x = invoer.x - oude_x;
-		double delta_y = invoer.y - oude_y;
-		draaiing.y -= delta_x;
-		draaiing.x -= delta_y;
+		_draaiDelta.y -= invoer.x - oude_x;
+		_draaiDelta.x -= invoer.y - oude_y;
 		oude_x = invoer.x;
 		oude_y = invoer.y;
 	}
@@ -76,16 +77,33 @@ class Speler : Voorwerp {
 	import std.datetime;
 
 	override void denkStap(Duration deltaT) {
-		import std.math : abs;
+		import std.math;
 
 		double deltaSec = deltaT.total!"hnsecs"() / 10_000_000.0;
-		this.plek = plek + Mat!(3).draaiMy(draai.y).maal(verplaatsing * cast(nauwkeurigheid)(snelheid * deltaSec));
 
-		Vec!3 draaiDelta = draaiing * cast(nauwkeurigheid)(draaiSnelheid * deltaSec);
-		if (abs(draai.x + draaiDelta.x) > 3.14)
-			draaiDelta.x = 0;
-		this.draai = draai + draaiDelta;
-		draaiing = Vec!3(0);
+		Vec!3 vooruit = ydraai * Vec!3([0, 0, -1]);
+		Vec!3 rechts = ydraai * Vec!3([1, 0, 0]);
+
+		Mat!3 verplaatsMat;
+		verplaatsMat.zetKol(0, rechts);
+		verplaatsMat.zetKol(1, Vec!3([0, 1, 0]));
+		verplaatsMat.zetKol(2, vooruit);
+
+		this.plek = this.plek + cast(Vec!3)(
+			verplaatsMat ^ (_verplaatsing * cast(nauw)(snelheid * deltaSec)));
+
+		_draaiDelta = _draaiDelta * cast(nauw)(draaiSnelheid * deltaSec);
+		_draai = _draai + _draaiDelta;
+		if (abs(_draai.x) > PI_2)
+			_draai.x = sgn(_draai.x) * PI_2;
+		if (abs(_draai.y) > PI)
+			_draai.y -= sgn(_draai.y) * 2 * PI;
+
+		xdraai = Quat.draai(Vec!3([1, 0, 0]), _draai.x);
+		ydraai = Quat.draai(Vec!3([0, 1, 0]), _draai.y);
+		this.draai = ydraai * xdraai;
+
+		_draaiDelta = Vec!2(0);
 
 		super.denkStap(deltaT);
 	}
